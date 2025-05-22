018fc1c7246f357b5474b277f0e8826d779db2f8
jabes <jabesgames@gmail.com>
2025-05-22 14:11:22 +0300

    init commit 2

---
+++ src/main/java/ru/jabes/flat_rent_new/FlatRentNewApplication.java
@@ -0,0 +1,13 @@
+package ru.jabes.flat_rent_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatRentNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatRentNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jabes/flat_rent_new/FlatRentNewApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jabes.flat_rent_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatRentNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
