e9b2b3e182e32854307d480d3052de90def0d3d4
jabes <jabesgames@gmail.com>
2025-05-27 11:44:21 +0300

    task - 5.1 ok

---
+++ src/main/java/ru/jabes/flat_rent_new/controller/ApartmentController.java
@@ -0,0 +1,23 @@
+package ru.jabes.flat_rent_new.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.jabes.flat_rent_new.dto.ApartmentDto;
+import ru.jabes.flat_rent_new.service.ApartmentService;
+
+@RestController
+@RequestMapping("/apartment")
+@RequiredArgsConstructor
+public class ApartmentController {
+    private final ApartmentService apartmentService;
+
+    @PostMapping
+    public ResponseEntity<ApartmentDto> createApartment(ApartmentDto apartmentDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(apartmentService.save(apartmentDto));
+    }
+}
